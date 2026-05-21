dfd7d08e67bc723bffbcf7be30a56048f9ab700f
Remco Huijsen <r.huijsen@tudelft.nl>
2026-05-21 14:02:22 +0200

    Adapted diffusion x U-Net example notebook to generate 64x64 toy images

--- diffuser/unet_example_config.py
+++ diffuser/unet_example_config.py
@@ -9,17 +9,17 @@ class DiffuserConfig:
     ###################
 
     ### UNET Parts Config ###
-    down_block_types: Tuple = ("AttnDown", "AttnDown", "AttnDown", "Down") # AttnDown/Down
+    down_block_types: Tuple = ("AttnDown", "AttnDown", "AttnDown") # AttnDown/Down
     mid_block_types: str = "AttnMid"                                       # AttnMid/Mid
-    up_block_types: Tuple = ("Up", "AttnUp", "AttnUp", "AttnUp")           # AttnUp/Up
-    unet_channels_per_block = (64, 128, 256, 256)
+    up_block_types: Tuple = ("Up", "AttnUp", "AttnUp")           # AttnUp/Up
+    unet_channels_per_block = (64, 128, 256)
     unet_residual_layers_per_block: int = 2
     unet_up_down_factor: int = 2
     unet_up_down_kernel_size: int = 3
 
     ### Time Embeddings Config ###
-    time_embed_start_dim: int = 160
-    time_embed_proj_dim: int = 640
+    time_embed_start_dim: int = 64
+    time_embed_proj_dim: int = 256
 
     ### Attention Config ###
     attention_head_dim: int = 64
